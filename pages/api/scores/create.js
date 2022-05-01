22481b93b5f38a240063abd463ce1749e1b52eec
terr97us <terr97us@gmail.com>
2022-05-01 23:43:28 +0600

    first commit

---
+++ pages/api/scores/create.js
@@ -0,0 +1,14 @@
+import Redis from "ioredis";
+
+const redis = new  Redis(process.env.REDIS_URL);
+
+export default async function handler(req, res) {
+  const { name, score } = req.body;
+
+  // console.log({ name, score });
+  await redis.zadd("scores", score, name);
+  const rank = await redis.zrevrank("scores", name,);
+  // const rank = await redis.zrank("scores", name);
+
+  res.status(200).json({ success: true, rank });
+}
